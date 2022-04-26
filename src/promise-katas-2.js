68628a5154836b7339da9b0b370f44d8a1d6a660
Ed Adkins <ejmadkins@gmail.com>
2022-04-26 21:08:08 +0100

    add katas

--- src/promise-katas-2.js
+++ src/promise-katas-2.js
@@ -25,33 +25,51 @@ fetch("joke", "question").then(<insert your callback function>)
 // 1 Create a function that uses the fetch function to make a request to the "food" URL and returns
 // the data - expected return value "Cheese" of type String
 
-const food = () => {};
+const food = () => fetch("food").then(({ data }) => data);
 
 // 2 Create a function that uses the fetch function to make a request to the "cats" URL and returns
 // a list of cats in alphabetical order - expected return value ["Bandit", "Berry", "Puss in boots", "Smokey"] of type Array
 
-const cat = () => {};
+const cat = () => fetch("cats").then(({ data }) => data.cats.sort());
 
 // 3 Create a function that uses the fetch function to make a request to the "dogs" URL and returns
 // the naughtiest dog - expected return value {name: "Mutley", naughty: 10} of type Object
 
-const dog = () => {};
+const dog = () =>
+  fetch("dogs").then(({ data }) =>
+    data.dogs.reduce((previousValue, currentValue) =>
+      previousValue.naughty > currentValue.naughty
+        ? previousValue
+        : currentValue
+    )
+  );
 
 // 4 Create a function that uses the fetch function to make requests to the "jokes" URL and returns
-// a joke object with the key of question and answer - expected return { 
+// a joke object with the key of question and answer - expected return {
 //     question: "Why did the scarecrow win the Nobel Prize?",
 //     answer: "Because he was out-standing in his field."
 // } of type Object
-// You will have to make more than one request to our fakeApi to get all the data you need. 
+// You will have to make more than one request to our fakeApi to get all the data you need.
 // Be aware of nesting your calls (try to avoid callback hell). Take a look at Promise.all on MDN
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
 //
 
-const joke = () => {};
+const joke = () => {
+  const questionAnswer = {};
+  return fetch("jokes", "question")
+    .then((response) => {
+      questionAnswer.question = response.joke;
+      return fetch("jokes");
+    })
+    .then((response) => {
+      questionAnswer.answer = response.answer;
+      return questionAnswer;
+    });
+};
 
 module.exports = {
-    food,
-    cat,
-    dog,
-    joke
-}
+  food,
+  cat,
+  dog,
+  joke,
+};
